96c7ff0accb6216a6d80fb8ecf8233636fd799ad
Karina <alvarez-webdev@gmail.com>
2021-03-20 16:12:10 -0400

    commit changes

---
+++ README.md
@@ -0,0 +1,5 @@
+# README #
+
+### What is this repository for? ###
+
+This repository was intended for the Bootstrap4 course in Coursera. This project is linked with the Honor certification of the Bootstrap course
---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*======
+; TYPEWRITTER EFFECT
+; Author: W.S. Toh
+; Modified by: Karina Alvarez
+*/
+
+var tw = {
+    container: "typewriterSub", // ID of typewriter container
+    text: [ // Blocks of text to show
+      "A wife",
+      "A mother of four",
+      "A year-round homeschool mom",
+      "A pet foster mom",
+      "A student at Bellevue University",
+      "A future full-stack developer"
+  
+    ],
+    delay: 100,
+    blockDelay: 800,
+  
+    timer: null, // Used to hold the timer
+    pointer: 0, // text position
+    block: 0, // block of text
+    draw: function() {
+      tw.pointer++;
+      tw.container.innerHTML = tw.text[tw.block].substring(0, tw.pointer);
+      if (tw.pointer < tw.text[tw.block].length) {
+        tw.timer = setTimeout(tw.draw, tw.delay);
+      }
+  
+      // End of block
+      else {
+        tw.block++;
+        if (tw.text[tw.block] == undefined) {
+          tw.block = 0;
+        }
+        tw.timer = setTimeout(tw.reset, tw.blockDelay);
+      }
+    },
+    reset: function() {
+      tw.pointer = 0;
+      tw.container.innerHTML = "";
+      tw.timer = setTimeout(tw.draw, tw.delay);
+    }
+  };
+  
+  window.addEventListener("load", function() {
+    tw.container = document.getElementById(tw.container);
+    tw.draw();
+  });
